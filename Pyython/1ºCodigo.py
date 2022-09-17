b99265d86ab3b39c9b82a3e54dbb07139e8d8fda
andonivila <andonivila@gmail.com>
2022-09-17 12:47:30 +0200

    Update 1ºCodigo.py

--- Pyython/1ºCodigo.py
+++ Pyython/1ºCodigo.py
@@ -3,6 +3,40 @@ Esto es un comentario
 Especialmente largo
 '''
 
-numero=3
-mensaje=(f"Hola Juan tu numero es {numero}")
+'''
+bool 
+int numero entero
+float numero decimal
+complex numero complejo
+str cadena de texto
+lista [..,..]
+tuple (..,..)
+dic {key:value, key:value}
+set 
+frozenset set que no pude variar 
+range
+
+'''
+
+
+
+'''
+BOOL
+'''
+from turtle import st
+
+
+casado : bool= True
+print (f'Casado? {casado}')
+
+
+'''
+Lista
+'''
+
+miLista :[str] =['Martin', 'Juan' , 'Pablo']
+'''
+el * te permite imprimir tan solo los elementos de la lista
+'''
+print(miLista[1])
 
