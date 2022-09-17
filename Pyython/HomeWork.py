5ddf83441a5ea0f8fc4eaf5c76ef1d8ef9e9333a
andonivila <andonivila@gmail.com>
2022-09-17 14:02:49 +0200

    update

---
+++ Pyython/HomeWork.py
@@ -0,0 +1,39 @@
+'''
+Ejemplo de input
+name = input ("Dime tu nombre: ")
+
+print (f"tu nombre es {name}")
+
+'''
+
+'''
+Cambiar de numero o string a un entero
+
+'''
+''' 
+edad = int(input("Cuales es tu edad "))
+
+print (f"Hola tu edad es de {edad}")
+
+'''
+
+''' 
+EJERCICIO PARA CASA 
+
+Una consola que pida
+
+Nombre 
+Email
+Contraseña
+Edad
+y aceptar el entrar
+
+(pensar que datos son cada elemento)
+
+Con todos los elementos tiene que crear un usuario (diccionario)
+
+y mostrar los datos por consola al finalizar
+
+'''
+
+
