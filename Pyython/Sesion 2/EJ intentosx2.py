c6b56c901b604c64b1f262a51fbd1cb4a17d029e
andonivila <andonivila@gmail.com>
2022-09-23 11:01:19 +0200

    update

---
+++ Pyython/Sesion 2/EJ intentosx2.py
@@ -0,0 +1,22 @@
+numero_elegido = int(input("Escoge un numero: ")) 
+numero_secreto=300
+
+numero_intentos=3
+
+
+
+while numero_elegido != numero_secreto and numero_intentos>1:
+    if numero_elegido<numero_secreto:
+        print ("El numero secreto es más grande")
+        numero_intentos=numero_intentos-1 
+        print (f"Te quedan {numero_intentos} intentos")
+        numero_elegido = int(input("Escoge un numero: "))
+
+    else:
+        print ("El numero secreto es mas pequeño")
+        numero_intentos=numero_intentos-1
+        print (f"Te quedan {numero_intentos} intentos")
+        numero_elegido = int(input("Escoge un numero: ")) 
+
+print (f"Perfecto el numero era {numero_secreto}")
+
