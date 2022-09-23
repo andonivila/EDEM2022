e38352dad7e812a84558cdcae18b333c2651c08f
andonivila <andonivila@gmail.com>
2022-09-23 19:00:44 +0200

    Update

--- Pyython/Sesion 3/CalculadoraBisiestos.py
+++ Pyython/Sesion 3/CalculadoraBisiestos.py
@@ -1,17 +1,18 @@
-anio=int(input("Pon un año"))
+#anio=int(input("Pon un año "))
+from multiprocessing.resource_sharer import stop
 
 
+anio=2000
+def bisiesto (a) :
+     a=anio%4
+     b=anio%100
+     c=anio%400
 
-def bisiesto (a: int) :
-    a=a%4
-    
-    if a==0:
-        a=a%100
-    if a==0:
-     a=a%400
-    if a==0:
-     print (f"Es un año bisiesto")
-    else:
-        print("No es un año bisiesto")
-
+     if a==0:
+        if b==0:
+            if c==0:
+                print("Es un año bisiesto")
+            else:
+                print("No es un año bisiesto")
+        
 bisiesto (anio)
