1095fb6a13db1ba3c363c65671aeebfad95b9582
andonivila <andonivila@gmail.com>
2022-09-28 00:36:23 +0200

    Update

---
+++ Pyython/Ejercicios/DIFICILES/MIEJ8.py
@@ -0,0 +1,38 @@
+
+from pprint import pprint
+a=list(range(1,10))
+largo=len(a)
+pprint(a)
+
+mul1=a[largo-1]
+mul2=a[largo-2]
+resul=mul1*mul2
+i=-2
+i=i-1
+mul2=a[largo+i]
+resul=resul*mul2
+
+
+
+
+
+'''
+#Sistema de bucle
+largo=len(a)
+i=0
+while i<largo :
+    print ("Anto")
+    i=i+1
+    b=a[1]
+
+
+b=(a[5])
+c=(a[1])
+
+d=b*c
+
+print(b)
+print(c)
+
+print(d)
+'''
