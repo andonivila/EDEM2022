dbe8c99c3ed27b6b7aaf483ca65a171d4c10c97c
andonivila <andonivila@gmail.com>
2022-10-26 16:17:25 +0200

    update

---
+++ FUNDAMENTOS/Pyython/Ejercicios/Obligatorios/aaa.py
@@ -0,0 +1,5 @@
+lista=[{'a':'1','b':2}]
+
+
+if '1' in lista:
+    print('hola')
---
+++ a.py
@@ -0,0 +1,8 @@
+lista=[{'nombre':'anto','apellido':'vila'}]
+
+
+
+a=range (10)
+for a  in lista:
+    
+    print (lista[a])
